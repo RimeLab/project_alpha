284bd7d1c84dd2cc207d5123616a020cbf025c73
Frost <edwarddenicolas@gmail.com>
2026-05-18 22:07:08 +1000

    update

---
+++ AlphaApi/AppMetadata.cs
@@ -0,0 +1,4 @@
+public class AppMetadata
+{
+    public string Version { get; set; } = "v0.0.1";
+}
--- AlphaApi/Program.cs
+++ AlphaApi/Program.cs
@@ -1,6 +1,12 @@
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<AppMetadata>(builder.Configuration.GetSection("AppMetadata"));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => new { message = "Welcome" });
+app.MapGet("/metadata", (IOptions<AppMetadata> metadata) => metadata.Value);
 
 app.Run();
